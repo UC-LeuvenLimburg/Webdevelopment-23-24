5c1acee84be3d6c2872e9ec66c120986c44f52d0
MimiWillems <mimi.willems@ucll.be>
2024-03-11 13:20:46 +0100

    Update Pizza.js na 2e les

--- Javascript/Pizza HTML/Pizza.js
+++ Javascript/Pizza HTML/Pizza.js
@@ -1,4 +1,4 @@
-var pizzas = { 
+var pizzas = {
     "margherita": {
         // naam: "margherita",
         prijs: 10,
@@ -11,27 +11,34 @@ var pizzas = {
     }
 }
 
-var winkelmandje = [];
+var ingredienten = ["tomatensaus", "mozerella", "hesp", "ananas"];
 
-function addItemToSelect(idName, text){
+var winkelmandje = {};
+
+function addItemToSelect(idName, text) {
     let option = document.createElement("option");
     option.innerHTML = text;
-    document.getElementById(idName).appendChild(option); 
+    document.getElementById(idName).appendChild(option);
 }
 
 
-function addList(){
-    for (pizza in pizzas){
+function addList() {
+    for (pizza in pizzas) {
         console.log(pizzas[pizza]);
         // console.log(pizzas[pizza].naam);
         // console.log(pizzas[pizza]["naam"]);
         addItemToSelect("pizzaLijst", pizza);
-    } 
+    }
+
+    for (ingredient of ingredienten){
+        console.log(ingredient);
+        addItemToSelect("ingredientenLijst", ingredient);
+    }
 }
 
 addList();
 
-document.getElementById("pizzaLijst").addEventListener("change", function(){
+document.getElementById("pizzaLijst").addEventListener("change", function () {
     let pizza = document.getElementById("pizzaLijst").value;
     console.log(pizzas[pizza].prijs);
     document.getElementById("showPizzaName").innerHTML = pizza.toUpperCase();
@@ -39,15 +46,44 @@ document.getElementById("pizzaLijst").addEventListener("change", function(){
     document.getElementById("showPizzaDetails").innerHTML += "Indrediënten: " + pizzas[pizza].ingredienten.join(", ");
 });
 
-document.getElementById("orderPizza").addEventListener("click", function(){
-    let aantal = +document.getElementById("aantal").value; 
+document.getElementById("orderPizza").addEventListener("click", function () {
+    let aantal = +document.getElementById("aantal").value;
     let pizza = document.getElementById("pizzaLijst").value;
-    if (isNaN(aantal) || aantal <= 0)  {
+    if (isNaN(aantal) || aantal <= 0) {
         alert("Geef een positief aantal in.");
     } else if (pizza == undefined || pizza == "") {
-          alert("Duid een pizza aan.");
+        alert("Duid een pizza aan.");
     } else {
-        winkelmandje[pizza] = {aantal: aantal, kostprijs: pizzas[pizza].prijs*aantal}
+        if (winkelmandje[pizza]) {
+            aantal += winkelmandje[pizza].aantal;
+        }
+        winkelmandje[pizza] = { aantal: aantal, naam: pizza, kostprijs: pizzas[pizza].prijs * aantal }
         console.log(winkelmandje);
+        toonWinkelmandje();
     }
 })
+
+function toonWinkelmandje() {
+    let tabel = document.getElementById("winkelmandje");
+    let totaalRij = document.getElementById("totaalrij");
+    while (tabel.children.length > 2) {
+        tabel.removeChild(tabel.children[1]);
+    }
+
+    var totaal = 0;
+    for (let pizzaRij in winkelmandje) {
+        console.log(pizzaRij);
+        let rij = document.createElement("tr");
+        for (let pizzaCel in winkelmandje[pizzaRij]) {
+            console.log(pizzaCel);
+            let cel = document.createElement("td");
+            cel.innerHTML = winkelmandje[pizzaRij][pizzaCel];
+            rij.appendChild(cel);
+        }
+
+        tabel.insertBefore(rij, totaalRij); 
+    
+        totaal += winkelmandje[pizzaRij].kostprijs;     
+    }
+    document.getElementById("totaal").innerHTML = totaal;
+}
