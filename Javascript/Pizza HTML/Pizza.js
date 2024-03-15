b80067577a620f01b4b766cb83be0aa91d46f65d
MimiWillems <mimi.willems@ucll.be>
2024-03-15 19:14:28 +0100

    Toch fouten na merge voor pizza.js

--- Javascript/Pizza HTML/Pizza.js
+++ Javascript/Pizza HTML/Pizza.js
@@ -1,261 +1,186 @@
-// Ingrediënten, pizzas en winkelmandje zijn globale objecten
+// Pizzas, Ingrediënten en winkelmandje zijn globale objecten (gebruik var)
 // zo zijn ze eigenlijk eigenschappen van de pagina
-// We voegen al een paar ingrediënten en pizza's toe bij opstarten.
+// We voegen al een paar pizza's en ingrediënten toe bij opstarten.
 // Het winkelmandje is leeg bij opstarten. 
-
-var ingredients=["tomatensaus","hesp","mozarella","ananas"];
 var pizzas = {
-	Margherita:{
-		// name: "Margherita",
-		prijs: 10.5,
-		ingredienten:["tomatensaus","mozarella"]
-	},
-	Hawai:{
-		// name: "Hawai",
-		prijs: 12.0,
-		ingredienten:["tomatensaus", "mozarella", "hesp", "ananas"]
-	}
-};
-var winkelmandje = [];
+    "margherita": {
+        // naam: "margherita",
+        prijs: 10,
+        ingredienten: ["tomatensaus", "mozzarella"]
+    },
+    hawai: {
+        // naam: "hawai",
+        prijs: 12,
+        ingredienten: ["tomatensaus", "mozzarella", "hesp", "ananas"]
+    }
+}
+
+var ingredienten = ["tomatensaus", "mozzarella", "hesp", "ananas"];
+
+var winkelmandje = {};
 
 //We voeren bij opstarten de functie addList uit
-//om bij opstarten de hierboven gemaakte pizzas en ingrediënten te tonen
+//om bij opstarten de hierboven samengestelde pizzas en ingrediënten te tonen
 addList();
 
-//De functie voegt een option toe aan de select
+// --FUNCTIES-- //
+//De functie addItemToSelect voegt een option toe aan de select
 // Het eerste argument is het id van de lijst.
 // Het tweede argument is de tekst die toegevoegd moet worden
-function addItemToSelect(selectName, optionName){
-	var select = document.getElementById(selectName);
-	var option = document.createElement("option");
-	option.text = optionName; //kan ook met innerHTML of createTextNode
-	select.add(option);  //en appendChild
+function addItemToSelect(idName, text) {
+    let option = document.createElement("option");
+    option.innerHTML = text;
+    document.getElementById(idName).appendChild(option);
 }
 
-//De functie vult de 2 lijsten met: 
+//De functie addList vult de 2 lijsten met: 
 // - de pizza's die in het object pizzas zitten
-// - de ingrediënten die in de array ingredients zitten
-function addList(){
-	for (var pizza in pizzas){
-		// addItemToSelect("pizzaLijst", pizzas[pizza].name);
-		addItemToSelect("pizzaLijst", pizza);
-		//pizza is de eerste keer "Margherita" en de tweede keer "Hawai"
-		//pizza is dus een String die de naam van de pizzas voorstelt
-		//Omdat pizza een Object is (en geen Array)
-		// bestaat pizzas.length niet of geeft dit de waarde 0
-	}
-	
-	// -- 4 wijzes om een array waardes toe te voegen --
-	// voor een array kunnen we niet werken met for in
-	// we kunnen wel een for maken aan de hand van de lengte van de array
-	// 	i is dus een getal, van 0 tot de lengte-1
-	// for (var i=0; i<ingredients.length; i++){
-	// 	addItemToSelect("ingredientenLijst", ingredients[i]);
-	// 	
-	// }
+// - de ingrediënten die in de array ingredienten zitten
+function addList() {
+    for (let pizza in pizzas) {
+        console.log(pizzas[pizza]);
+        // console.log(pizzas[pizza].naam);
+        // console.log(pizzas[pizza]["naam"]);
+        addItemToSelect("pizzaLijst", pizza);
+    }
 
-	// ingredients.forEach(function(ingredient){
-	// 	addItemToSelect("ingredientenLijst", ingredient);
-	// });
-
-	// ingredients.forEach(ingredient => {addItemToSelect("ingredientenLijst", ingredient)});
-
-	for (ingredient of ingredients){
-		addItemToSelect("ingredientenLijst", ingredient);
-	}
+    for (let ingredient of ingredienten){
+        console.log(ingredient);
+        addItemToSelect("ingredientenLijst", ingredient);
+    }
 }
 
-//De functie leest het (globaal) object winkelmandje
-//en toont een rij voor elke pizza in het winkelmandje
-//update ook de totaalprijs van het winkelmandje
-function toonWinkelmandje(){
-	var tabel = document.getElementById("winkelmandje");
-	var totaalrij = document.getElementById("totaalrij");
-		
-	// verwijder eerst de huidige info
+// De functie toonWinkelmandje
+// een rij voor elke pizza in het globaal object winkelmandje.
+// een rij bestaat uit meerder cellen afhankelijk van de eigenschappen van elke pizza in het object winkelmandje
+// als laatste wordt de totaalprijs berekend en getoond
+function toonWinkelmandje() {
+    let tabel = document.getElementById("winkelmandje");
+    let totaalRij = document.getElementById("totaalrij");
+
+    // verwijder eerst de huidige info
 	// de eerste en laatste rij mogen niet verwijdert worden
-	while (tabel.children.length > 2){
-		tabel.removeChild(tabel.children[1]);  
-	}
-	
-	//Om het totaal te berekenen moeten we het eerst initialiseren
-	var totaal = 0;
-	//Voor elke pizza in het winkelmandje
-	for (var pizza in winkelmandje){
-		//verhogen we het totaal met het subtotaal van de pizza
-		totaal += winkelmandje[pizza].kostprijs
-		//maken we een rij met elk 3 kolommen
-		//elke kolom heeft een textnode
-		var rij = document.createElement("tr");  
-		var aantalKolom = document.createElement("td");
-		var naamKolom = document.createElement("td");
-		var subtotaalKolom = document.createElement("td");
-		var aantalText = document.createTextNode(winkelmandje[pizza].aantal); 
-		var naamText = document.createTextNode(pizza); 
-		var prijsText = document.createTextNode(winkelmandje[pizza].kostprijs); 
-		//voeg de textnodes toe aan de kolommen
-		aantalKolom.appendChild(aantalText);
-		naamKolom.appendChild(naamText);
-		subtotaalKolom.appendChild(prijsText);
-		/* OF
-		aantalKolom.innerHTML = winkelmandje[pizza].aantal;
-		naamKolom.innerHTML = pizza;
-		subtotaalKolom.innerHTML = winkelmandje[pizza].kostprijs;
-		*/
-		//voeg de kolommen toe aan de rij
-		rij.appendChild(aantalKolom);
-		rij.appendChild(naamKolom);
-		rij.appendChild(subtotaalKolom);
-		//voeg de rij toe aan de tabel
-		tabel.insertBefore(rij, totaalrij); 
-	}
-	// Wijzig het nieuwe berekende totaal
-	document.getElementById("totaal").innerHTML = totaal;
+    while (tabel.children.length > 2) {
+        tabel.removeChild(tabel.children[1]);
+    }
+
+    // initialiseer totaal voordat je het met de kostprijs van elke pizzarij verhoogt
+    var totaal = 0;
+
+    // voor elke pizza in het object winkelmandje
+    for (let pizzaRij in winkelmandje) {
+        console.log(pizzaRij);
+        let rij = document.createElement("tr");
+        // voor elke eigenschap in het object pizzaRij in winkelmandje
+        for (let pizzaCel in winkelmandje[pizzaRij]) {
+            console.log(pizzaCel);
+            // maak een cel met de waarde van elke eigenschap van de pizzaRij
+            let cel = document.createElement("td");
+            cel.innerHTML = winkelmandje[pizzaRij][pizzaCel];
+            // voeg de cel toe aan de rij
+            rij.appendChild(cel);
+        }
+        // toon de rij voor de totaalrij
+        tabel.insertBefore(rij, totaalRij); 
+        // verhoog het totaal met de kostprijs van de pizzaRij
+        totaal += winkelmandje[pizzaRij].kostprijs;     
+    }
+    // update het totaal
+    document.getElementById("totaal").innerHTML = totaal;
 }
 
 // --EVENTS-- //
-//Wanneer we de pizzalijst wijzigen of met andere woorden
-//een pizza in de lijst aangeduid wordt 
-//tonen we de details van deze pizza
-document.getElementById("pizzaLijst").addEventListener("change", function() {
-	//var pizzalijst = document.getElementById("pizzaLijst"); 
-	//pizzaLijst kan in dit event vervangen worden door this
-	//omdat dit het element is waarop het event wordt toegepast
-	
-	//pizzaNaam is de string "Hawai" of "Margherita"
-	var pizzaNaam = this.value;
-					//this.options[this.selectedIndex].value;
+// Wanneer we de pizzalijst wijzigen of met andere woorden
+// een pizza in de lijst aangeduid wordt 
+// tonen we de details van deze pizza
+document.getElementById("pizzaLijst").addEventListener("change", function () {
+    let pizza = document.getElementById("pizzaLijst").value;
+    				//this.options[this.selectedIndex].value;
 	  				//omdat we maar 1 item selecteren in de lijst, 
 	  				//geven beide hetzelfde
-				
-	//Toon geselecteerde pizzaNaam, prijs van geselecteerde pizza en lijst van ingrdeiënten van geselecteerde pizza
-	document.getElementById("showPizzaName").innerHTML = pizzaNaam;
-	document.getElementById("showPizzaDetails").innerHTML = "Prijs: " + pizzas[pizzaNaam].prijs + "€</br>";
-	document.getElementById("showPizzaDetails").innerHTML += "Ingrediënten: " + pizzas[pizzaNaam].ingredienten.join(', ');
+    console.log(pizzas[pizza].prijs);
+    document.getElementById("showPizzaName").innerHTML = pizza.toUpperCase();
+    document.getElementById("showPizzaDetails").innerHTML = "Prijs: " + pizzas[pizza].prijs + "€<br>";
+    document.getElementById("showPizzaDetails").innerHTML += "Indrediënten: " + pizzas[pizza].ingredienten.join(", ");
 });
 
-//Wanneer de knop Bestel Pizza gedrukt wordt
-//moeten we pizza's toevoegen aan het object winkelmandje
-//en het winkelmandje tonen
-//Door dit op te slaan in het object winkelmandje 
-// weten we welke pizza's al getoond zijn. 
-document.getElementById("orderPizza").addEventListener("click", function() {
-	//Lees aantal
-	var aantal = +document.getElementById("aantal").value;
-	//Lees de geselecteerd pizza
-	//var pizzalijst = document.getElementById("pizzaLijst");
-	var selectedPizza = document.getElementById("pizzaLijst").value;	
-	// pizzalijst.options[pizzalijst.selectedIndex].value;
-	// Controleer of aantal een positief getal is 
-	if (isNaN(aantal) || aantal <= 0){
-        alert("Vul het aantal pizzas dat je wilt bestellen in.");
-	//Controleer of de geselecteerde pizza bestaat 
-	} else if (!pizzas[selectedPizza]) {
-	//} else if (pizzalijst.selectedIndex <= -1){
-        alert("Selecteer de pizza die je wil bestellen.");
-	} else {
-		// zit deze pizza al in het winkelmandje?
-		if (winkelmandje[selectedPizza]) {
-		// dan verhogen we aantal en subtotaal
-		winkelmandje[selectedPizza].aantal += aantal;
-        winkelmandje[selectedPizza].kostprijs += aantal*pizzas[selectedPizza].prijs; 
-		} else {
-		// zo niet, dan voegen we deze toe, met zijn aantal en subtotaal
-		winkelmandje[selectedPizza] = {
-			aantal: aantal, 
-			kostprijs: aantal*pizzas[selectedPizza].prijs
-			};
-		}
-		console.log(winkelmandje);
-		toonWinkelmandje();	
-	} 
-});
+// Wanneer de knop Bestel Pizza gedrukt wordt
+// controleren we of alle verlden ingevuld werd voordat 
+// we een pizza kunnen toevoegen aan het object winkelmandje
+// en het winkelmandje tonen
+// Wanneer de pizza al in winkelmandje zit, passen we de gegevens aan
+document.getElementById("orderPizza").addEventListener("click", function () {
+    let aantal = +document.getElementById("aantal").value;
+    let pizza = document.getElementById("pizzaLijst").value;
+    // let pizzalijst = document.getElementById("pizzaLijst")
+    // let pizza = pizzalijst.options[pizzalijst.selectedIndex].value;
+    if (isNaN(aantal) || aantal <= 0) {
+        alert("Geef een positief aantal in.");
+    } else if (pizza == undefined || pizza == "") {
+    //} else if (!pizzas[pizza]) {
+    //} else if (pizzalijst.selectedIndex <= -1){
+        alert("Duid een pizza aan.");
+    } else {
+        // zit deze pizza al in het winkelmandje?
+        if (winkelmandje[pizza]) {
+            // dan tellen we het eerder toegevoegde aantal erbij
+            aantal += winkelmandje[pizza].aantal;
+        }
+        // en voegen de pizza toe of passen de pizza aan
+        winkelmandje[pizza] = { aantal: aantal, naam: pizza, kostprijs: pizzas[pizza].prijs * aantal }
+        console.log(winkelmandje);
+        toonWinkelmandje();
+    }
+})
 
-
-//Wanneer de knop Add extra Pizza gedrukt wordt
-//stellen we een nieuwe pizza samen
-//voegen deze toe aan het object pizzas
-//en tonen deze in de selectielijst
+// Wanneer de knop Add extra Pizza gedrukt wordt
+// controleren we of alle verlden ingevuld werd voordat 
+// we een nieuwe pizza samenstellen
+// en deze toevoegen aan het object pizzas
+// en tonen in de selectielijst
 document.getElementById("addPizza").addEventListener("click", function() {
-	//Lees de ingevulde naam en prijs
-	var pizzaNaam = document.getElementById("pizzaName").value;
-	var pizzaPrijs = +document.getElementById("pizzaPrijs").value;
-	//Neem alle opties uit de lijst
-	//Wanneer ze geselecteerd zijn, voeg je dan toe aan een nieuwe array
-	var ingredientenLijst = document.getElementById("ingredientenLijst").options;
-	var selectedIngredienten = [];
-	
-	// for (var i = 0; i < ingredientenLijst.length; i++){
-	// 	if (ingredientenLijst[i].selected){
-	// 		selectedIngredienten.push(ingredientenLijst[i].value);
-	// 	}
-	// }
+	let pizzaNaam = document.getElementById("pizzaName").value;
+	let pizzaPrijs = +document.getElementById("pizzaPrijs").value;
+    let selectedIngredienten = [];
+    let selectedOptions = document.getElementById("ingredientenLijst").selectedOptions;
+	for (let option of selectedOptions){
+		selectedIngredienten.push(option.value);
+	}
+    // selectedIngredienten = Array.from(selectedOptions).map(option => option.value);
 
+	// OF we nemen eerst alle opties uit de lijst en kijken dan voor elk item of het geselecteerd is
+	// let ingredientenLijst = document.getElementById("ingredientenLijst").options;
 	// for (ingredient of ingredientenLijst){
 	// 	if (ingredient.selected){
 	// 		selectedIngredienten.push(ingredient.value);
 	// 	}
 	// }
-	//Let op: Hier kan je geen .forEach, .filter of .map gebruiken omdat ingredientenLijst geen array is
-	// maar wel een HTMLOptionsCollection
-	// via volgende omweg zou het wel kunnen: 
-	var selectedOptions = document.getElementById("ingredientenLijst").selectedOptions;
-	//selectedIngredienten = Array.from(selectedOptions).map(option => option.value);
-	//OF 
-	for (option of selectedOptions){
-		selectedIngredienten.push(option.value);
-	}
-	
-	//Controleer de ingave
+
 	if (pizzaNaam.length < 1 || isNaN(pizzaPrijs) || pizzaPrijs <= 0 || selectedIngredienten.length <= 0) {
 		alert("De gegevens om een pizza toe te voegen zijn niet volledig of niet correct.");
-	// controleer of de pizza al bestaat.
 	} else if(pizzas[pizzaNaam]){
 		alert("Deze pizza bestaat al.");
 	} else {
-	// wanneer de ingave correct is
-	// voeg een nieuwe pizza toe aan het object pizzas
-	// en voeg het toe aan de pizza selectielijst
 		pizzas[pizzaNaam] = {
 			prijs: pizzaPrijs, 
 			ingredienten: selectedIngredienten
 			};
 		addItemToSelect("pizzaLijst", pizzaNaam);
-	}
-	
+	}	
 });
 
-//Wanneer de knop Add extra ingrediënt gedrukt wordt
-//voegen dit toe aan de array ingredients
-//en tonen deze in de selectielijst
+// Wanneer de knop Add extra ingrediënt gedrukt wordt
+// voegen we dit toe aan de array ingredients
+// en tonen deze in de selectielijst
 document.getElementById("addIngredient").addEventListener("click", function() {
-	var nieuwIngredient = document.getElementById("ingredient").value;
+	let nieuwIngredient = document.getElementById("ingredient").value;
 	
-	//controleer of een ingredient werd ingegeven
 	if (nieuwIngredient.length <= 0){
 		alert("Geef ingredient in.");
 	} else {
 		//controleer of het ingredient al bestaat
 		var bestaatAl = false;
-		// for (var i = 0; i<ingredients.length; i++){
-		// 	if (nieuwIngredient == ingredients[i]){
-		// 		alert("ingredient bestaat al");
-		// 		bestaatAl = true;
-		// 	}
-		// }
-		// ingredients.forEach(function(ingredient){
-		// 	if (nieuwIngredient == ingredient){
-		// 		alert("ingredient bestaat al");
-		//  		bestaatAl = true;
-		// 	}
-		// });
-		// ingredients.forEach((ingredient) => {
-		// 	if (nieuwIngredient == ingredient){
-		// 		alert("ingredient bestaat al");
-		// 		bestaatAl = true;
-		// 	}
-		// });
-		for (ingredient of ingredients){
+		for (let ingredient of ingredienten){
 			if (nieuwIngredient == ingredient){
 				alert("ingredient bestaat al");
 				bestaatAl = true;
@@ -263,7 +188,7 @@ document.getElementById("addIngredient").addEventListener("click", function() {
 		}
 
 		if (!bestaatAl){
-			ingredients.push(nieuwIngredient);
+			ingredienten.push(nieuwIngredient);
 			addItemToSelect("ingredientenLijst", nieuwIngredient);
 		}
 	}
